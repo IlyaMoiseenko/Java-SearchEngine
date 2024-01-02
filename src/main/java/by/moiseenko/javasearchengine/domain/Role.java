ece575cd049ec8711ab1b97d64dbc2022c2557df
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2024-01-02 18:37:50 +0300

    add: domain

---
+++ src/main/java/by/moiseenko/javasearchengine/domain/Role.java
@@ -0,0 +1,11 @@
+package by.moiseenko.javasearchengine.domain;
+
+/*
+    @author Ilya Moiseenko on 2.01.24
+*/
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ src/main/java/by/moiseenko/javasearchengine/domain/User.java
@@ -0,0 +1,42 @@
+package by.moiseenko.javasearchengine.domain;
+
+/*
+    @author Ilya Moiseenko on 2.01.24
+*/
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstname;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastname;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+}
