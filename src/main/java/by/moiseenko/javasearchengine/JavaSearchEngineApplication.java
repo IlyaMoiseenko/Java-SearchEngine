1b921cc2906f9f4d39c192a09e60353795f73b05
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2024-01-02 18:28:01 +0300

    initial commit

---
+++ src/main/java/by/moiseenko/javasearchengine/JavaSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package by.moiseenko.javasearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/moiseenko/javasearchengine/JavaSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package by.moiseenko.javasearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
