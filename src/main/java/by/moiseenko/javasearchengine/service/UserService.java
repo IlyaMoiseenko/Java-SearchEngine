4e2f4d391eb33f0b0b7be14f18c15e90d47f64d9
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2024-01-03 19:42:52 +0300

    add: ability to add new site for indexing

---
+++ src/main/java/by/moiseenko/javasearchengine/service/UserService.java
@@ -0,0 +1,25 @@
+package by.moiseenko.javasearchengine.service;
+
+/*
+    @author Ilya Moiseenko on 3.01.24
+*/
+
+import by.moiseenko.javasearchengine.domain.User;
+import by.moiseenko.javasearchengine.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
